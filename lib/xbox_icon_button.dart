38efd383bb397d4388dec0f567a7b4be16c9737a
Zonaro <zonaro@outlook.com>
2023-10-13 00:01:27 -0300

    -

--- lib/xbox_icon_button.dart
+++ lib/xbox_icon_button.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:xbox_ui/xbox_colors.dart';
 
 class XboxIconButton extends StatefulWidget {
-  const XboxIconButton({super.key, this.onPressed, required this.icon, this.selectedIcon});
+  const XboxIconButton({super.key, this.onPressed, required this.icon, this.selectedIcon, this.size = 10});
 
   final void Function()? onPressed;
 
   final Widget icon;
   final Widget? selectedIcon;
+  final double size;
 
   @override
   State<XboxIconButton> createState() => _XboxIconButtonState();
@@ -18,25 +19,29 @@ class _XboxIconButtonState extends State<XboxIconButton> {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      type: MaterialType.transparency,
-      child: Ink(
-        decoration: BoxDecoration(
-          border: Border.all(color: hasFocus ? XboxColors.currentAccentColor : Colors.transparent, width: 4.0),
-          color: Theme.of(context).colorScheme.background,
-          shape: BoxShape.circle,
-        ),
-        child: InkWell(
-          splashColor: XboxColors.currentAccentColor,
-          onFocusChange: (v) {
-            setState(() {
-              hasFocus = v;
-            });
-          },
-          //This keeps the splash effect within the circle
-          borderRadius: BorderRadius.circular(double.infinity), //Something large to ensure a circle
-          onTap: widget.onPressed,
-          child: Padding(padding: const EdgeInsets.all(20.0), child: hasFocus ? (widget.selectedIcon ?? widget.icon) : widget.icon),
+    return SizedBox(
+      width: widget.size,
+      height: widget.size,
+      child: Material(
+        type: MaterialType.transparency,
+        child: Ink(
+          decoration: BoxDecoration(
+            border: Border.all(color: hasFocus ? XboxColors.currentAccentColor : Colors.transparent, width: 4.0),
+            color: Theme.of(context).colorScheme.background,
+            shape: BoxShape.circle,
+          ),
+          child: InkWell(
+            splashColor: XboxColors.currentAccentColor,
+            onFocusChange: (v) {
+              setState(() {
+                hasFocus = v;
+              });
+            },
+            //This keeps the splash effect within the circle
+            borderRadius: BorderRadius.circular(double.infinity), //Something large to ensure a circle
+            onTap: widget.onPressed,
+            child: Padding(padding: const EdgeInsets.all(20.0), child: hasFocus ? (widget.selectedIcon ?? widget.icon) : widget.icon),
+          ),
         ),
       ),
     );

b6ed0a814febf38d2f0efbb824cac740fa7cee55
Zonaro <zonaro@outlook.com>
2023-10-13 01:29:11 -0300

    -

--- test/xbox_ui_test.dart
+++ test/xbox_ui_test.dart
@@ -3,6 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:xbox_ui/xbox_colors.dart';
 import 'package:xbox_ui/xbox_dashboard.dart';
 import 'package:xbox_ui/xbox_icon_button.dart';
+import 'package:xbox_ui/xbox_menu.dart';
 import 'package:xbox_ui/xbox_tile.dart';
 import 'package:xbox_ui/xbox_tile_list.dart';
 import 'package:xbox_ui/xbox_tile_view.dart';
@@ -22,17 +23,24 @@ class XboxTester extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Xbox Demo',
         theme: XboxColors.getTheme(),
         home: XboxDashboard(
+          userdetail: 'XboxUser',
+          username: '1234 points',
+          menu: const XboxMenu(
+            items: [
+              ListTile(title: Text("First")),
+              ListTile(title: Text("Second")),
+              ListTile(title: Text("Third")),
+            ],
+          ),
           topBarItens: const [
             XboxIconButton(icon: Icon(Icons.music_note)),
             XboxIconButton(icon: Icon(Icons.book)),
             XboxIconButton(icon: Icon(Icons.access_time_filled_outlined)),
             XboxIconButton(icon: Icon(Icons.settings)),
           ],
-          userdetail: 'XboxUser',
-          username: '1234 points',
           child: XboxTileView(items: [
             XboxTileList(tiles: [
               XboxTile.icon(icon: Icons.settings, title: "Settings", width: 100, height: 100, iconSize: 30),

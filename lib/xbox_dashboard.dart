f0bdbcd128ffa992d8ef1955c1e8c85389ca4de8
Zonaro <zonaro@outlook.com>
2023-10-13 00:43:09 -0300

    -

--- lib/xbox_dashboard.dart
+++ lib/xbox_dashboard.dart
@@ -33,50 +33,47 @@ class _XboxDashboardState extends State<XboxDashboard> {
             centerTitle: true,
             surfaceTintColor: Colors.transparent,
             leadingWidth: MediaQuery.of(context).size.width * 0.3,
-            leading: GestureDetector(
-              onTap: () => Scaffold.of(context).openDrawer(),
-              child: Padding(
-                padding: const EdgeInsets.only(left: 20, top: 20),
-                child: Wrap(
-                  spacing: 10,
-                  children: [
-                    CircleAvatar(
-                      backgroundColor: XboxColors.currentAccentColor,
-                      //child: Text("${initials.first.first()}${initials.skip(1).lastOrNull.ifBlank("")?.last()}"),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(100),
-                        child: widget.avatar,
+            automaticallyImplyLeading: true,
+            leading: Padding(
+              padding: const EdgeInsets.only(left: 20, top: 20),
+              child: Wrap(
+                spacing: 10,
+                children: [
+                  CircleAvatar(
+                    backgroundColor: XboxColors.currentAccentColor,
+                    //child: Text("${initials.first.first()}${initials.skip(1).lastOrNull.ifBlank("")?.last()}"),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(100),
+                      child: widget.avatar,
+                    ),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      AutoSizeText(
+                        widget.username,
+                        maxLines: 1,
+                        minFontSize: 4,
+                        maxFontSize: 20,
+                        style: const TextStyle(fontWeight: FontWeight.bold),
                       ),
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        AutoSizeText(
-                          widget.username,
-                          maxLines: 1,
-                          minFontSize: 4,
-                          maxFontSize: 20,
-                          style: const TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        AutoSizeText(
-                          widget.userdetail,
-                          maxLines: 1,
-                          minFontSize: 2,
-                          maxFontSize: 15,
-                          style: const TextStyle(fontWeight: FontWeight.bold),
-                        )
-                      ],
-                    ),
-                  ],
-                ),
+                      AutoSizeText(
+                        widget.userdetail,
+                        maxLines: 1,
+                        minFontSize: 2,
+                        maxFontSize: 15,
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                ],
               ),
             ),
             toolbarHeight: 85,
             backgroundColor: Colors.transparent,
             shadowColor: Colors.transparent,
             elevation: 0,
-            automaticallyImplyLeading: false,
             title: Row(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.center,

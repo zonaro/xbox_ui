cec63a53b2c25f8075fd41e5df79bc3584d6d7df
Zonaro <zonaro@outlook.com>
2023-10-13 00:54:10 -0300

    -

--- lib/xbox_dashboard.dart
+++ lib/xbox_dashboard.dart
@@ -21,11 +21,13 @@ class XboxDashboard extends StatefulWidget {
 class _XboxDashboardState extends State<XboxDashboard> {
   @override
   Widget build(BuildContext context) {
+    final scaffoldKey = GlobalKey<ScaffoldState>();
     return Stack(
       fit: StackFit.expand,
       children: [
         if (widget.wallpaper != null) SizedBox(width: MediaQuery.of(context).size.width, child: widget.wallpaper),
         Scaffold(
+          key: scaffoldKey,
           backgroundColor: Theme.of(context).colorScheme.background.withOpacity(.7),
           drawer: widget.menu,
           extendBody: true,
@@ -34,40 +36,43 @@ class _XboxDashboardState extends State<XboxDashboard> {
             surfaceTintColor: Colors.transparent,
             leadingWidth: MediaQuery.of(context).size.width * 0.3,
             automaticallyImplyLeading: true,
-            leading: Padding(
-              padding: const EdgeInsets.only(left: 20, top: 20),
-              child: Wrap(
-                spacing: 10,
-                children: [
-                  CircleAvatar(
-                    backgroundColor: XboxColors.currentAccentColor,
-                    //child: Text("${initials.first.first()}${initials.skip(1).lastOrNull.ifBlank("")?.last()}"),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(100),
-                      child: widget.avatar,
-                    ),
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      AutoSizeText(
-                        widget.username,
-                        maxLines: 1,
-                        minFontSize: 4,
-                        maxFontSize: 20,
-                        style: const TextStyle(fontWeight: FontWeight.bold),
+            leading: GestureDetector(
+              onTap: () => scaffoldKey.currentState?.openDrawer(),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20, top: 20),
+                child: Wrap(
+                  spacing: 10,
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: XboxColors.currentAccentColor,
+                      //child: Text("${initials.first.first()}${initials.skip(1).lastOrNull.ifBlank("")?.last()}"),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(100),
+                        child: widget.avatar,
                       ),
-                      AutoSizeText(
-                        widget.userdetail,
-                        maxLines: 1,
-                        minFontSize: 2,
-                        maxFontSize: 15,
-                        style: const TextStyle(fontWeight: FontWeight.bold),
-                      )
-                    ],
-                  ),
-                ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        AutoSizeText(
+                          widget.username,
+                          maxLines: 1,
+                          minFontSize: 4,
+                          maxFontSize: 20,
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        AutoSizeText(
+                          widget.userdetail,
+                          maxLines: 1,
+                          minFontSize: 2,
+                          maxFontSize: 15,
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
             toolbarHeight: 85,
--- lib/xbox_ui.dart
+++ lib/xbox_ui.dart
@@ -1,3 +1,7 @@
 library xbox_ui;
+ 
 
+
+
+ 
  

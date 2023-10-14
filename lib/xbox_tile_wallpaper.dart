2974f7f2bd1963d6a225d96cade153d75be4e381
Zonaro <zonaro@outlook.com>
2023-10-14 16:18:03 -0300

    -

--- lib/xbox_tile_wallpaper.dart
+++ lib/xbox_tile_wallpaper.dart
@@ -12,8 +12,6 @@ class XboxFadeInRadialWallpaper extends StatefulWidget {
 }
 
 class _XboxFadeInRadialWallpaperState extends State<XboxFadeInRadialWallpaper> {
-  double opacityLevel = 0.0;
-
   @override
   void initState() {
     super.initState();
@@ -21,36 +19,31 @@ class _XboxFadeInRadialWallpaperState extends State<XboxFadeInRadialWallpaper> {
 
   @override
   Widget build(BuildContext context) {
-    var duration = const Duration(milliseconds: 1500);
+    var duration = const Duration(milliseconds: 3);
     return Stack(
       fit: StackFit.expand,
       children: [
-        SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: AnimatedCrossFade(
-            firstChild: widget.oldTileWallpaper ?? widget.dashboardWallpaper ?? const SizedBox.shrink(),
-            secondChild: widget.newTileWallpaper ?? widget.dashboardWallpaper ?? const SizedBox.shrink(),
-            crossFadeState: widget.newTileWallpaper != null ? CrossFadeState.showSecond : CrossFadeState.showFirst,
-            duration: duration,
-          ),
+        AnimatedCrossFade(
+          firstChild: widget.oldTileWallpaper ?? widget.dashboardWallpaper ?? const SizedBox.expand(),
+          secondChild: widget.newTileWallpaper ?? widget.dashboardWallpaper ?? const SizedBox.expand(),
+          crossFadeState: widget.newTileWallpaper != null ? CrossFadeState.showSecond : CrossFadeState.showFirst,
+          duration: duration,
         ),
-        if (widget.newTileWallpaper != null)
-          AnimatedCrossFade(
-            duration: duration,
-            crossFadeState: widget.newTileWallpaper == null && widget.oldTileWallpaper == null ? CrossFadeState.showFirst : CrossFadeState.showSecond,
-            firstChild: const SizedBox.shrink(),
-            secondChild: Container(
-              decoration: BoxDecoration(
-                gradient: RadialGradient(
-                  center: Alignment.center,
-                  radius: 1.0,
-                  colors: [Colors.transparent, Theme.of(context).colorScheme.background],
-                  stops: const [0.5, 1.0],
-                ),
+        AnimatedCrossFade(
+          duration: duration,
+          crossFadeState: widget.newTileWallpaper == null && widget.oldTileWallpaper == null ? CrossFadeState.showFirst : CrossFadeState.showSecond,
+          firstChild: const SizedBox.shrink(),
+          secondChild: Container(
+            decoration: BoxDecoration(
+              gradient: RadialGradient(
+                center: Alignment.center,
+                radius: 1.0,
+                colors: [Colors.transparent, Theme.of(context).colorScheme.background],
+                stops: const [0.5, 1.0],
               ),
             ),
           ),
+        ),
       ],
     );
   }

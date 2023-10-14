8d9416adefd8f3cdd9f72d2e110e6bbbf2916372
Zonaro <zonaro@outlook.com>
2023-10-14 16:04:22 -0300

    -

--- lib/xbox.dart
+++ lib/xbox.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: constant_identifier_names, non_constant_identifier_names
 
-import 'package:flutter/foundation.dart';
+ 
 import 'package:flutter/material.dart';
 
 import 'package:xbox_ui/xbox_tile.dart';

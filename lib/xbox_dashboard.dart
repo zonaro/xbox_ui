799e845c8ce5aa955ee1e72f8a8e40d23bfdd0f2
Zonaro <zonaro@outlook.com>
2023-10-16 07:42:45 -0300

    -

--- lib/xbox_dashboard.dart
+++ lib/xbox_dashboard.dart
@@ -8,7 +8,7 @@ import 'package:xbox_ui/xbox_menu.dart';
 import 'xbox.dart';
 
 class XboxDashboard extends StatefulWidget {
-  const XboxDashboard({super.key, required this.child, required this.topBarItens, this.avatar, this.menu, required this.username, required this.userdetail});
+  XboxDashboard({super.key, required this.child, required this.topBarItens, this.avatar, this.menu, required this.username, required this.userdetail});
 
   final List<Widget> topBarItens;
 
@@ -19,13 +19,24 @@ class XboxDashboard extends StatefulWidget {
   final String username;
   final String userdetail;
 
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
+  showHomeMenu() {
+    debugPrint("showing home menu");
+    if (scaffoldKey.currentState != null) {
+      if (scaffoldKey.currentState!.isDrawerOpen) {
+        scaffoldKey.currentState!.closeDrawer();
+      } else {
+        scaffoldKey.currentState!.openDrawer();
+      }
+    }
+  }
+
   @override
   State<XboxDashboard> createState() => _XboxDashboardState();
 }
 
 class _XboxDashboardState extends State<XboxDashboard> {
-  final scaffoldKey = GlobalKey<ScaffoldState>();
-
   @override
   initState() {
     Xbox.tileWallpaperNotifier.addListener(() {
@@ -49,132 +60,121 @@ class _XboxDashboardState extends State<XboxDashboard> {
     super.initState();
   }
 
-  _showHomeMenu() {
-    debugPrint("showing home menu");
-    if (scaffoldKey.currentState != null) {
-      if (scaffoldKey.currentState!.isDrawerOpen) {
-        scaffoldKey.currentState!.closeDrawer();
-      } else {
-        scaffoldKey.currentState!.openDrawer();
-      }
-    }
-  }
-
   var keyboardNode = FocusNode(descendantsAreFocusable: true);
 
   @override
   Widget build(BuildContext context) {
-    return KeyboardListener(
-      focusNode: keyboardNode,
-      onKeyEvent: keyEvents,
-      child: Stack(
-        fit: StackFit.expand,
-        children: [
-          AnimatedSwitcher(
-            duration: const Duration(seconds: 1),
-            child: Container(
-              key: ValueKey(Random().nextInt(9999)),
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height,
-              decoration: BoxDecoration(
-                image: DecorationImage(image: Xbox.tileWallpaper ?? Xbox.userWallpaper ?? Xbox.emptyImage, fit: BoxFit.cover, alignment: Alignment.center),
-                gradient: RadialGradient(
-                  center: Alignment.center,
-                  radius: 1.0,
-                  colors: [Colors.transparent, Theme.of(context).colorScheme.background],
-                  stops: const [0.5, 1.0],
-                ),
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        AnimatedSwitcher(
+          duration: const Duration(seconds: 1),
+          child: Container(
+            key: ValueKey(Random().nextInt(9999)),
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+              image: DecorationImage(image: Xbox.tileWallpaper ?? Xbox.userWallpaper ?? Xbox.emptyImage, fit: BoxFit.cover, alignment: Alignment.center),
+              gradient: RadialGradient(
+                center: Alignment.center,
+                radius: 1.0,
+                colors: [Colors.transparent, Theme.of(context).colorScheme.background],
+                stops: const [0.5, 1.0],
               ),
             ),
           ),
-          Scaffold(
-            key: scaffoldKey,
-            backgroundColor: Theme.of(context).colorScheme.background.withOpacity(.7),
-            drawer: widget.menu,
-            extendBody: true,
-            appBar: AppBar(
-              centerTitle: true,
-              surfaceTintColor: Colors.transparent,
-              leadingWidth: MediaQuery.of(context).size.width * 0.3,
-              automaticallyImplyLeading: true,
-              leading: Padding(
-                padding: const EdgeInsets.only(left: 20, top: 20),
-                child: Wrap(
-                  spacing: 10,
-                  children: [
-                    XboxCircleButton(
-                      onPressed: () => _showHomeMenu(),
-                      size: 50,
-                      backgroundColor: Xbox.accentColorNotifier.value,
-                      child: widget.avatar ?? const Icon(Icons.person),
+        ),
+        Scaffold(
+          key: widget.scaffoldKey,
+          backgroundColor: Theme.of(context).colorScheme.background.withOpacity(.7),
+          drawer: widget.menu,
+          extendBody: true,
+          appBar: AppBar(
+            centerTitle: true,
+            surfaceTintColor: Colors.transparent,
+            leadingWidth: MediaQuery.of(context).size.width * 0.3,
+            automaticallyImplyLeading: true,
+            leading: Padding(
+              padding: const EdgeInsets.only(left: 20, top: 20),
+              child: Wrap(
+                spacing: 10,
+                children: [
+                  XboxCircleButton(
+                    onPressed: () => widget.showHomeMenu(),
+                    size: 50,
+                    backgroundColor: Xbox.accentColorNotifier.value,
+                    child: widget.avatar ?? const Icon(Icons.person),
+                  ),
+                  FittedBox(
+                    fit: BoxFit.contain,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          widget.username,
+                          maxLines: 1,
+                          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                        ),
+                        Text(
+                          widget.userdetail,
+                          maxLines: 1,
+                          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                        )
+                      ],
                     ),
-                    FittedBox(
-                      fit: BoxFit.contain,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            widget.username,
-                            maxLines: 1,
-                            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                          ),
-                          Text(
-                            widget.userdetail,
-                            maxLines: 1,
-                            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
-                          )
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-              toolbarHeight: 85,
-              backgroundColor: Colors.transparent,
-              shadowColor: Colors.transparent,
-              elevation: 0,
-              title: SizedBox(
+            ),
+            toolbarHeight: 85,
+            backgroundColor: Colors.transparent,
+            shadowColor: Colors.transparent,
+            elevation: 0,
+            title: SizedBox(
+              width: MediaQuery.of(context).size.width * .3,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: widget.topBarItens,
+              ),
+            ),
+            actions: [
+              SizedBox(
                 width: MediaQuery.of(context).size.width * .3,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: widget.topBarItens,
-                ),
-              ),
-              actions: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * .3,
-                )
-              ],
-            ),
-            body: widget.child,
+              )
+            ],
           ),
-        ],
-      ),
+          body: widget.child,
+        ),
+      ],
     );
   }
 
   keyEvents(KeyEvent event) {
     if (event is KeyDownEvent) {
       if (event.logicalKey == LogicalKeyboardKey.meta || event.logicalKey == (LogicalKeyboardKey.gameButtonMode) || event.logicalKey == (LogicalKeyboardKey.escape) || event.logicalKey == (LogicalKeyboardKey.goBack)) {
-        _showHomeMenu();
+        widget.showHomeMenu();
       }
 
       if (event.logicalKey == LogicalKeyboardKey.arrowDown) {
+        debugPrint("GOING DOWN");
         keyboardNode.focusInDirection(TraversalDirection.down);
         return;
       }
       if (event.logicalKey == LogicalKeyboardKey.arrowUp) {
+        debugPrint("GOING UP");
         keyboardNode.focusInDirection(TraversalDirection.up);
         return;
       }
       if (event.logicalKey == LogicalKeyboardKey.arrowLeft) {
+        debugPrint("GOING LEFT");
         keyboardNode.focusInDirection(TraversalDirection.left);
         return;
       }
 
-        if (event.logicalKey == LogicalKeyboardKey.arrowRight) {
+      if (event.logicalKey == LogicalKeyboardKey.arrowRight) {
+        debugPrint("GOING RIGHT");
         keyboardNode.focusInDirection(TraversalDirection.right);
         return;
       }
